90acb88aaa867eba3cfa660884c07f5d71c31be2
Gustavo Rodrigo Gularte Arend <gustavorrarend@gmail.com>
2023-08-11 20:58:07 -0300

    .

---
+++ exercicios/DOM para iniciantes/o-que-e-dom.js
@@ -0,0 +1,14 @@
+// Retorne o url da página atual utilizando o objeto window
+const url = window.location.href;
+
+// Seleciona o primeiro elemento da página que
+// possua a classe ativo
+const ativo = document.querySelector('.ativo');
+console.log(ativo)
+
+// Retorne a linguagem do navegador
+const linguagem = window.navigator.language
+
+// Retorne a largura da janela 
+const width = window.innerWidth
+console.log(width)
