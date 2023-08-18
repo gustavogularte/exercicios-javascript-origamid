e13217bfeb3d5ea0f0341bafcbfe210ee44bd6b5
Gustavo Rodrigo Gularte Arend <gustavorrarend@gmail.com>
2023-08-17 22:50:13 -0300

    seleção de elementos

--- exercicios/DOM para iniciantes/selecao-de-elementos.js
+++ exercicios/DOM para iniciantes/selecao-de-elementos.js
@@ -1,11 +1,19 @@
 // Retorne no console todas as imagens do site
-const imgs = document.querySelectorAll('.imgs');
+const imgs = document.querySelectorAll('img');
+console.log(imgs);
 
 // Retorne no console apenas as imagens que começaram com a palavra imagem
+const imgs2 = document.querySelectorAll('[src^="img/imagem"');
+console.log(imgs2);
 
 // Selecione todos os links internos (onde o href começa com #)
+const linkInterno = document.querySelectorAll('[href^="#"');
+console.log(linkInterno);
 
 // Selecione o primeiro h2 dentro de .animais-descricao
+const primeiroh2 = document.querySelector('.animais-descricao h2');
+console.log(primeiroh2);
 
 // Selecione o último p do site
-
+const ultimoP = document.querySelectorAll('p');
+console.log(ultimoP[--ultimoP.length]);
