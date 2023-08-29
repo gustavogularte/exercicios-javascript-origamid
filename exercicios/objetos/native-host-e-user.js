eb72e4e2d19e8470fb75d9a198e25028aeb03e64
Gustavo Rodrigo Gularte Arend <gustavorrarend@gmail.com>
2023-08-28 22:32:56 -0300

    native, host e user

---
+++ exercicios/objetos/native-host-e-user.js
@@ -0,0 +1,8 @@
+// Liste 5 objetos nativos
+//Resposta: Number, Math, Array, Function, Date
+
+// Liste 5 objetos do browser
+//Resposta: Window, Document, NodeList, location, HTMLCollection
+
+// Liste 2 Métodos, Propriedades ou Objetos
+// presentes no Chrome que não existem no Firefox
