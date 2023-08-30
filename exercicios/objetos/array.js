e9055d5931eeabf4ebee0047a9e1e39e0c035843
Gustavo Rodrigo Gularte Arend <gustavorrarend@gmail.com>
2023-08-30 19:11:55 -0300

    array

---
+++ exercicios/objetos/array.js
@@ -0,0 +1,37 @@
+const comidas = ['Pizza', 'Frango', 'Carne', 'Macarrão'];
+// Remova o primeiro valor de comidas e coloque em uma variável
+const primerioValor = comidas.shift();
+// Remova o último valor de comidas e coloque em uma variável
+const ultimoValor = comidas.pop();
+// Adicione 'Arroz' ao final da array
+comidas.push('Arroz');
+// Adicione 'Peixe' e 'Batata' ao início da array
+comidas.unshift('Peixe', 'Batata')
+
+const estudantes = ['Marcio', 'Brenda', 'Joana', 'Kleber', 'Julia'];
+// Arrume os estudantes em ordem alfabética
+estudantes.sort();
+// Inverta a ordem dos estudantes
+estudantes.reverse();
+// Verifique se Joana faz parte dos estudantes
+estudantes.includes('Joana');
+// Verifique se Juliana faz parte dos estudantes
+estudantes.includes('Juliana');
+
+let html = `<section>
+              <div>Sobre</div>
+              <div>Produtos</div>
+              <div>Contato</div>
+            </section>`
+// Substitua section por ul e div com li,
+// utilizando split e join
+html = html.split('section');
+html = html.join('ul');
+html = html.split('div');
+html = html.join('li');
+
+const carros = ['Ford', 'Fiat', 'VW', 'Honda'];
+// Remova o último carro, mas antes de remover
+// salve a array original em outra variável
+const carrosCopia = carros.slice();
+carros.pop();
