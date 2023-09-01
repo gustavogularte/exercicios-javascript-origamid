7d2a0dcee9f0bcc0d07a33d1cdb93dc9eb76bc3b
Gustavo Rodrigo Gularte Arend <gustavorrarend@gmail.com>
2023-08-31 23:39:16 -0300

    function

---
+++ exercicios/objetos/function/function.js
@@ -0,0 +1,25 @@
+// Retorne a soma total de caracteres dos
+// parágrafos acima utilizando reduce
+const paragrafos = document.querySelectorAll('p');
+const paragrafosArray = Array.from(paragrafos);
+const caracteres = paragrafosArray.reduce((acc, item) => {
+  return acc + item.innerText.length
+}, 0)
+console.log(caracteres)
+
+// Crie uma função que retorne novos elementos
+// html, com os seguintes parâmetros
+// tag, classe e conteudo.
+function criarElemento(tag, classe, conteudo) {
+  const elemento = document.createElement(tag);
+  classe ? elemento.classList.add(classe) : null;
+  conteudo ? elemento.innerHTML = conteudo : null;
+  return elemento;
+}
+console.log(criarElemento('h1', 'teste', 'testeste'))
+
+// Crie uma nova função utilizando a anterior como base
+// essa nova função deverá sempre criar h1 com a
+// classe titulo. Porém o parâmetro conteudo continuará dinâmico
+const h1Titulo = criarElemento.bind(null, 'h1', 'titulo');
+const cursosHTML = h1Titulo('Cursos de HTML');
