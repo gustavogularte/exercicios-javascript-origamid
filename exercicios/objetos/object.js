1b52261d54da66128624e9c4e92f9a14d167ee8b
Gustavo Rodrigo Gularte Arend <gustavorrarend@gmail.com>
2023-09-15 22:50:42 -0300

    object

--- exercicios/objetos/object.js
+++ exercicios/objetos/object.js
@@ -1,17 +1,17 @@
 // Crie uma função que verifique
 // corretamente o tipo de dado
-function verificarDado(dado) {
+function tipo(dado) {
   return Object.prototype.toString.call(dado);
 }
 
 // Crie um objeto quadrado com
 // a propriedade lados e torne
 // ela imutável
-const quadrado = {};
+const quadrado = {}
 Object.defineProperties(quadrado, {
   lados: {
     value: 4,
-    enumerable: true,
+    enumarable: true,
   }
 })
 
@@ -22,10 +22,10 @@ const configuracao = {
   height: 600,
   background: '#333'
 }
-Object.freeze(configuracao);
+Object.freeze(configuracao)
 
 // Liste o nome de todas
 // as propriedades do
 // protótipo de String e Array
-console.log(Object.getOwnPropertyNames(String.prototype));
-console.log(Object.getOwnPropertyNames(Array.prototype));
+const stringPropriedades = Object.getOwnPropertyNames(String.prototype);
+const arrayPropriedades = Object.getOwnPropertyNames(Array.prototype);
